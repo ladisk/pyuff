4be83ad6b32225acf3965602450f78ddf9f8a893
Arjaan Buijk <arjaan.buijk@gmail.com>
2016-06-02 10:04:24 -0400

    test001
    Read & print all datasets in a UFF

---
+++ test001.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pyuff
+
+uff_file = pyuff.UFF('C:\BMs\Initialize-Simufact\RefResult-UNV\Tet157.unv')
+
+datasets_in_uff = uff_file.get_set_types()
+print('datasets in uff:')
+print(datasets_in_uff)
+
+# read and print all datasets
+count = 0
+for type in datasets_in_uff:
+    print('---------------------------------------------------------------')
+    print('dataset        : ' + str(count) )
+    print('dataset type   : ' + str(type) )
+    print('dataset content: ' )
+    dataset = uff_file.read_sets(count)
+    print(dataset)
+    count += 1
+    
+
