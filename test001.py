e0eb10436105080365de78aab3478185eda23c26
Arjaan Buijk <arjaan.buijk@gmail.com>
2016-06-03 16:19:11 -0400

    Support 2411
    Was already mostly there. Main thing was that date-set 2411 was stored
    as type 15 during reading. I changed this to type 2411 during reading.

--- test001.py
+++ test001.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyuff
 
-uff_file = pyuff.UFF('data/simufact_tet_elements.unv')
+uff_file = pyuff.UFF('data/fea_simufact_tet_elements.unv')
 
 datasets_in_uff = uff_file.get_set_types()
 print('datasets in uff:')
