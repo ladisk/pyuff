8e28ada129d681f7e6afcea3ba3408b4e2e42487
Martin Glesser <martin.glesser@eomys.com>
2021-03-17 18:20:24 +0100

    [CC] added np.arrays in test for homogeneity

--- tests/test_2412.py
+++ tests/test_2412.py
@@ -11,7 +11,7 @@ def test_read_2412():
     uff_ascii = pyuff.UFF('./data/mesh_Oros-modal_uff15_uff2412.unv')
     a = uff_ascii.read_sets(2)
     np.testing.assert_array_equal(a['nodes_nums'][0], np.array([1, 25, 48, 24]))
-    np.testing.assert_array_equal(a['nodes_nums'][-1],[50, 74, 73, 49])
+    np.testing.assert_array_equal(a['nodes_nums'][-1], np.array([50, 74, 73, 49]))
 
 if __name__ == '__main__':
     test_read_2412()
