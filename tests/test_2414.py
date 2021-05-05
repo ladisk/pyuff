4fa5785e3fda009357dd2521091ab3e74b2c0dbb
Janko Slavič <janko.slavic@gmail.com>
2021-05-05 15:48:53 +0200

    renaming unv to uff to be consistent with other filenames and the name of the package :)

--- tests/test_2414.py
+++ tests/test_2414.py
@@ -7,14 +7,14 @@ import sys, os
 import pyuff
 
 def test_read_2414():
-    uff_ascii = pyuff.UFF('./data/DS2414_disp_file.unv')
+    uff_ascii = pyuff.UFF('./data/DS2414_disp_file.uff')
     a = uff_ascii.read_sets(3)
     np.testing.assert_array_equal(a['freq'], np.array([100]))
     np.testing.assert_array_equal(a['x'][3], np.array([3.33652e-09-1j*9.17913e-13]))
 
 def test_write_2414():
     # Read dataset 2414 in test file
-    uff_ascii = pyuff.UFF('./data/DS2414_disp_file.unv')
+    uff_ascii = pyuff.UFF('./data/DS2414_disp_file.uff')
     a = uff_ascii.read_sets(3)
     # Test
     np.testing.assert_array_equal(a['freq'], np.array([100]))
