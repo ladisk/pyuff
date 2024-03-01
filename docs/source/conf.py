2fe239df373560828b5c93024ccde73f0dc62b92
klemengit <zaletelj.klemen@gmail.com>
2024-03-01 06:56:27 +0100

    docs bugfixing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     # 'rinoh.frontend.sphinx',
+    'sphinx_copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
