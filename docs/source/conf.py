006eb1ecf5b0998aeba926fb609b0bd500cd7580
klemengit <zaletelj.klemen@gmail.com>
2024-12-03 09:07:34 +0100

    fixing docs bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -186,7 +186,10 @@ epub_exclude_files = ['search.html']
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = ('https://docs.python.org/', None)
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+}
+
 
 # Autodoc options (to show __init__ function in docs)
 autodoc_default_options = {
